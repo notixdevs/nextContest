477ab75dac5cac5b1634e610f0857914bfd299b8
Shivam <kingshivam2004@gmail.com>
2025-01-29 20:57:58 +0530

    Added Report Issue Link

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="bg-white text-center text-gray-800 text-sm py-[6px] px-8 mt-0">
             <div className="flex justify-between items-center">
-                <a href="#" className="font-semibold hover:text-blue-500">
+                <a href="https://forms.gle/iM17sUSwyJ17ayDr6" className="font-semibold hover:text-blue-500">
                     Report Issue
                 </a>
                 <a href="#" className="font-semibold hover:text-blue-500">
